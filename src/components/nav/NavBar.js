8695cfe59b2b84239f6e405f463c28a5547e9dae
Andrew Herring <andrewoherring@gmail.com>
2018-12-05 15:17:01 -0600

    react settup

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+import { Icon, Button } from 'semantic-ui-react'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./NavBar.css"
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light light-blue flex-md-nowrap shadow">
+        <ul className="nav nav-pills">
+
+          <li className="nav-item">
+            <Link className="nav-link" to="/"><Icon name="user circle" size="large" />User Profiles</Link>
+          </li>
+
+          <li className="nav-item">
+            <Link className="nav-link" to="/"><Icon name="calendar alternate outline" size="large" />Events</Link>
+          </li>
+
+          <li className="nav-item">
+            <Link className="nav-link" to="/"><Icon name="newspaper outline" size="large" />News</Link>
+          </li>
+
+          <li className="nav-item">
+            <Link className="nav-link" to="/"><Icon name="comments outline" size="large" />Messages</Link>
+          </li>
+
+          <li className="nav-item">
+            <Link className="nav-link" to="/"><Icon name="bell outline" size="large" />To Do</Link>
+          </li>
+
+          
+
+        </ul>
+        <p id="navTagline">Welcome to <img id="react-img" src="../../../react.png"></img> Nutshell!</p>
+        <div className="logbtn">
+        <Button animated onClick={() => {
+          document.location.href = 'http://localhost:3000/login'
+        }}>
+          <Button.Content visible>Login</Button.Content>
+          <Button.Content hidden>
+            <Icon name='sign-in alternate' />
+          </Button.Content>
+        </Button>
+        <Button animated onClick={() => {
+          localStorage.clear("credentials")
+          document.location.href = 'http://localhost:3000'
+        }}>
+          <Button.Content visible>Logout</Button.Content>
+          <Button.Content hidden>
+            <Icon name='sign-out alternate' />
+          </Button.Content>
+        </Button>
+        </div>      
+        </nav>
+    )
+  }
+}
